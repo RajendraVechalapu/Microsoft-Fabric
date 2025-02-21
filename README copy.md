743faf8167b0570215727aa82e94d2e9b643b13e
Rajendra Vechalapu <vrsanaidu@gmail.com>
2025-02-22 08:25:33 +1300

    FullLoad SQL Scripts

---
+++ README copy.md
@@ -0,0 +1,16 @@
+python -m venv rajenv
+
+pip freeze > requirements.txt
+#ensure remove install from requirements.txt after freeze command
+
+.\rajenv\Scripts\activate
+$env:DATABASE_URL = "postgresql://rajpostgressql_user:QfEIQR6tHAfUWFSwIIUxhmlAd4ozL0lZ@dpg-cma94evqd2ns73bd3bmg-a.oregon-postgres.render.com/rajpostgressql"
+$env:FLASK_APP = "myapp"
+flask run --reload
+
+pip install -r requirements.txt
+
+
+--Uninstalls
+pip list
+pip freeze > unins ; pip uninstall -y -r unins ; del unins
---
+++ datamodelling/FullReload/[Landing].[CustomerData_FullReload].sql
@@ -0,0 +1,12 @@
+CREATE TABLE [Landing].[CustomerData_FullReload] 
+(
+    CustomerData_FullReloadKey INT IDENTITY(1,1),  -- Primary key with auto-increment
+    [CustomerID] INT NOT NULL,                    -- Customer ID
+    [CustomerName] NVARCHAR(100) NULL,            -- Customer Name
+    [Email] NVARCHAR(100) NULL,                   -- Email
+    [Phone] NVARCHAR(20) NULL,                    -- Phone
+    [City] NVARCHAR(50) NULL,                     -- City
+    [LastUpdated] DATETIME NULL,                  -- Last Updated timestamp
+    [CreatedAsAt(ETL)] DATETIME NULL,             -- ETL Created timestamp
+    [BatchID] INT NULL                            -- Batch ID
+)
